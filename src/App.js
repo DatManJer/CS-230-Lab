0a5c9cbf9fbbeb959ba9fc9ee7ffecd9e03f6b49
DatManJer <jka00003@mix.wvu.edu>
2024-02-21 15:24:51 -0500

    Initial Commit (FINAL)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <h1>CS 230L</h1>
         <h2>Section - 002</h2>
         <p>WVU ID: 800372017</p>
         <p>Hi, I am Jeremy Appiah</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
