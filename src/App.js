b47df43085fa3bac5147eefa1a3d9272e1b8500d
DatManJer <jka00003@mix.wvu.edu>
2024-02-28 17:26:01 -0500

    Card and Navbar Commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 
 import './App.css';
+import Navbar from './Navbar'; // Correct import statement
+import Card from './Card';
 
 function App() {
   return (
-    <div>
-      <header className="App-header">
-        <h1>CS 230L</h1>
-        <h2>Section - 002</h2>
-        <p>WVU ID: 800372017</p>
-        <p>Hi, I am Jeremy Appiah</p>
-      </header>
-    </div>
+    <div className="App-header">
+      <Navbar />
+    <h1>CS 230L</h1>
+    <h2>Section - 02</h2>
+    <p>WVU ID: 800372017</p>
+    <p>Hi, I am Jeremy Appiah</p>
+    <Card />
+  
+</div>
   );
 }
 
